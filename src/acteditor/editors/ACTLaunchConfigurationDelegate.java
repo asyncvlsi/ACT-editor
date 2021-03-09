de0e36389aa235634c269e55a20214c8ca0096f8
Mawuli Akpalu <mawuli.akpalu@yale.edu>
2021-03-08 20:12:30 -0500

    Base version with terminal, and outline bar

---
+++ src/acteditor/editors/ACTLaunchConfigurationDelegate.java
@@ -0,0 +1,20 @@
+package acteditor.editors;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.debug.core.ILaunch;
+import org.eclipse.debug.core.ILaunchConfiguration;
+import org.eclipse.debug.core.model.ILaunchConfigurationDelegate;
+
+public class ACTLaunchConfigurationDelegate implements ILaunchConfigurationDelegate {
+
+	@Override
+	public void launch(ILaunchConfiguration config,
+						String mode, ILaunch launch, IProgressMonitor monitor)
+			throws CoreException {
+		
+		
+	
+	}
+
+}
